979a89043f809e13a6b39532198ca8d8226b6793
Homura <bryceainsleysanchez@gmail.com>
2024-12-05 14:22:44 +0800

    Organize 2024 Day 5 Code

--- AoC-2024/Day-05/2024-05-2.py
+++ AoC-2024/Day-05/2024-05-2.py
@@ -1,17 +1,9 @@
-with open('Sample.txt', 'r') as f:
+with open('Input.txt', 'r') as f:
     txt = f.read()
 
-#Seems to be a total ordering
-#1176 -> 49 pages to print
-
 '''IDEA
-0th iff left on 49 pairs
-1st iff left on 48 pairs
-...
-
-0th iff right on 0 pairs
-1st iff right on 1 pair
-...
+(Guaranteed) in the middle iff it is forced to have 
+n//2 items on each side by the rules
 '''
 
 rules_txt,order_txt = txt.split('\n\n')
@@ -44,37 +36,7 @@ def solve(line) -> int:
             return k
     raise Exception('Answer is not unique')
     
-
 ans = 0
 for order in orders:
     ans += solve(order)
 print(ans)
-
-exit(0)
-
-left_count = {}
-right_count = {}
-for rule in rules:
-    l,r = rule
-    left_count[l] = left_count.get(l,0)+1
-    right_count[r] = right_count.get(r,0)+1
-
-# print(left_count)
-# print(right_count)
-# print(len(left_count))
-# print(rules[:10])
-
-print(left_count)
-print(right_count)
-
-for i in left_count:
-    print(i, left_count[i])
-
-n = len(left_count)+1
-order = [-1 for i in range(n)]
-for k in left_count:
-    order[n-1-left_count[k]] = k
-for k in right_count:
-    order[right_count[k]] = k
-
-print(order)
