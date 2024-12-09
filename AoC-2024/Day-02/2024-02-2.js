5d314f29cfbf5a6dc64ade6bbb49ca3c875a8c11
Homura <bryceainsleysanchez@gmail.com>
2024-12-10 01:19:25 +0800

    Solve 2024 Day 2 in Javascript

---
+++ AoC-2024/Day-02/2024-02-2.js
@@ -0,0 +1,61 @@
+// USE `let` OR DIE TO RANDOM GLOBAL VARIABLES
+// https://javascript.plainenglish.io/beware-of-unsafe-implicit-globals-in-javascript-f370ccca8fdb
+// https://stackoverflow.com/questions/29656267/remove-elements-from-javascript-array-by-position
+
+const fs = require('fs')
+txt = fs.readFileSync('Input.txt', 'utf8')
+lines = txt.split('\n')
+
+function works_1(line) {
+    diffs = []
+    for (let i = 0; i < line.length-1; i++) {
+        diffs[i] = line[i+1]-line[i]
+    }
+    m = Math.min(...diffs)
+    M = Math.max(...diffs)
+    if (-3 <= m && M <= -1) {
+        return true
+    }
+    if (1 <= m && M <= 3) {
+        return true
+    }
+    return false
+}
+
+function works_2(line) {
+    if (works_1(line)) {
+        return true
+    }
+    for (let i = 0; i < line.length; i++) {
+        //Copy the line except for the ith number
+
+        // NOTE: The below part gives the wrong answer and I hae no idea why
+        // let _line = []
+        // for (let j = 0; j < line.length-1; j++) {
+        //     if (j < i) {
+        //         _line[j] = line[i]
+        //     } else if (j >= i) {
+        //         _line[j] = line[i+1]
+        //     }
+        // }
+
+        _line = [...line]
+        _line.splice(i,1)
+
+        if (works_1(_line)) {
+            return true
+        }
+    }
+    return false
+}
+
+
+
+ans = 0
+for (line of lines) {
+    line = line.split(' ')
+    if (works_2(line)) {
+        ans += 1
+    }
+}
+console.log(ans)
