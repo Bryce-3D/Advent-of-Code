6d0ea2346fe38c619ada1d9c9557bce6b6dc38a8
Homura <bryceainsleysanchez@gmail.com>
2024-12-24 02:02:50 +0800

    Solve 2024 Day 23 Part 2 in Python

--- AoC-2024/Day-23/2024-23.py
+++ AoC-2024/Day-23/2024-23.py
@@ -37,5 +37,80 @@ def part_1(adj_list:dict[str,set[str]]) -> int:
         raise Exception(f'What ({ans} is not div by 6)')
     return ans//6
 
+def get_K3(adj_list:dict[str,set[str]]) -> set[tuple[int,int,int]]:
+    ans = set()
+    #For each pair of vertices
+    for v0 in adj_list:
+        for v1 in adj_list:
+            #Check neighbors
+            neigh0 = adj_list[v0]
+            neigh1 = adj_list[v1]
+            #Not connected -> Insta skip
+            if v1 not in neigh0:
+                continue
+            #Get common neighbors
+            for neigh in neigh0:
+                if neigh in neigh1:
+                    tri = sorted([v0,v1,neigh])
+                    ans.add(tuple(tri))
+    return ans
+
+def get_Knext(adj_list:dict[str,set[str]], 
+            Kns:set[tuple[int,...]]
+            ) -> set[tuple[int,...]]:
+    '''
+    Given an adjacency list and the set of all n-cliques, 
+    returns the set of all (n+1)-cliques.
+    '''
+    ans = set()
+    for Kn in Kns:
+        for v_new in adj_list:
+            works = True
+            for v in Kn:
+                if v_new not in adj_list[v]:
+                    works = False
+                    break
+            if works:
+                K_new = list(Kn)
+                K_new.append(v_new)
+                K_new.sort()
+                K_new = tuple(K_new)
+                ans.add(K_new)
+    return ans
+
+def part_2_testing(adj_list:dict[str,set[str]]):
+    '''It peaks at 6-cliques, likely brute forcable this way'''
+    K3s = get_K3(adj_list)
+    print(len(K3s))   #11011
+    K4s = get_Knext(adj_list, K3s)
+    print(len(K4s))   #26455
+    K5s = get_Knext(adj_list, K4s)
+    print(len(K5s))   #45045
+    K6s = get_Knext(adj_list, K5s)
+    print(len(K6s))   #55770
+    K7s = get_Knext(adj_list, K6s)
+    print(len(K7s))   #50622
+    K8s = get_Knext(adj_list, K7s)
+    print(len(K8s))   #33462
+
+def part_2(adj_list:dict[str,set[str]]) -> set[tuple[int,...]]:
+    Kns = get_K3(adj_list)
+    Knexts = get_Knext(adj_list, Kns)
+    while len(Knexts) > 0:
+        Kns = Knexts
+        Knexts = get_Knext(adj_list, Kns)
+    return Kns
+
+def part_2_parsed(adj_list:dict[str,set[str]]) -> list[str]:
+    Kns = part_2(adj_list)
+    ans = []
+    for Kn in Kns:
+        a = sorted(list(Kn))
+        s = ','.join(a)
+        ans.append(s)
+    return ans
+
 adj_list = parse_input('Input.txt')
+
 print(part_1(adj_list))
+print(part_2_parsed(adj_list))
